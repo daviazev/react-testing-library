805dd591a90029e5c11f1eefe1c3cdab2cb71c84
daviazevdo <azvd.davi@gmail.com>
2022-07-19 00:16:00 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,15 @@
-test('', () => {});
-// comentário pra fazer o primeiro commit
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+test('testa o componente About', () => {
+  renderWithRouter(<About />);
+
+  const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+  const heading = screen.getByRole('heading', { name: 'About Pokédex' });
+  const img = screen.getByRole('img');
+  expect(heading).toBeInTheDocument();
+  expect(img).toHaveAttribute('src', url);
+});
