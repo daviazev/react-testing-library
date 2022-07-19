a34aa670851e35dad47fb53265a7b59ce535a5e0
daviazevdo <azvd.davi@gmail.com>
2022-07-19 00:35:40 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+test('testa a page FavoritePokemons', () => {
+  renderWithRouter(<FavoritePokemons />);
+
+  const heading = screen.getByText(/No favorite pokemon found/i);
+  expect(heading).toBeInTheDocument();
+});
