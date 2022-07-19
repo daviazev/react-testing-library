985f3d034c943ef642a8e83723c5150dcf58f190
daviazevdo <azvd.davi@gmail.com>
2022-07-19 00:54:48 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+test('testa a page NotFound', () => {
+  renderWithRouter(<NotFound />);
+
+  const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+  const heading = screen.getByRole('heading', { name: /Page requested not found/i });
+  expect(heading).toBeInTheDocument();
+  const image = screen.getAllByRole('img')[1];
+  expect(image).toHaveAttribute('src', url);
+});
